63e619ad3ba9598e38594c71c23bc67ed2490c86
YassineCherkaoui <57252454+YassineCherkaoui@users.noreply.github.com>
2020-09-30 16:25:54 +0100

    Yalla

---
+++ DB/yalla_groupe.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 30, 2020 at 05:22 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `yalla_groupe`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `yalla-groupe`
+--
+-- Error reading structure for table yalla_groupe.yalla-groupe: #1932 - Table 'yalla_groupe.yalla-groupe' doesn't exist in engine
+-- Error reading data for table yalla_groupe.yalla-groupe: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `yalla_groupe`.`yalla-groupe`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
